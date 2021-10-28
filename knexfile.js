b155a236d0306f20e3935316b5ba1896e9fa146a
Martin Carson <87444494+Jumbaba@users.noreply.github.com>
2021-10-28 11:45:13 -0700

    initial upload

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+require("dotenv").config();
+
+const {
+  PRODUCTION_DATABASE_URL,
+  DEVELOPMENT_DATABASE_URL,
+} = process.env;
+
+const DATABASE_URL = process.env.NODE_ENV === "production" ? PRODUCTION_DATABASE_URL : DEVELOPMENT_DATABASE_URL;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
